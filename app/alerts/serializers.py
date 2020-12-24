42185cec0c7eee7637f0e20b5401dd8a25f9578e
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2020-12-24 16:33:46 +0530

    adding article model with serializer

--- app/alerts/serializers.py
+++ app/alerts/serializers.py
@@ -2,12 +2,8 @@ from rest_framework import serializers
 from alerts.models import Article
 
 
-class ArticleSerializers(serializers.Serializer):
+class ArticleSerializer(serializers.ModelSerializer):
     '''Serializer for the alert articles'''
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, max_length=100)
-    description = serializers.CharField()
-
-    def create(self, validated_data):
-        '''Create a new alert article given validated_data'''
-        return Article.objects.create(**validated_data)
+    class Meta:
+        model = Article
+        fields = ('title', 'description')
--- app/alerts/test/test_models.py
+++ app/alerts/test/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from alerts.serializers import ArticleSerializers
+from alerts.serializers import ArticleSerializer
+from alerts.models import Article
 
 
 class ModelTest(TestCase):
@@ -8,8 +9,9 @@ class ModelTest(TestCase):
         """Test creating new alert is successful"""
         title = 'some title'
         description = 'some description'
-        alrt = ArticleSerializers(title=title, description=description)
+        new_alert = Article(title=title, description=description)
+        new_alert.save()
+        alert_serializer = ArticleSerializer(new_alert)
 
-        self.assertEqual(alrt.title, title)
-        self.assertEqual(alrt.description, description)
-
+        self.assertEqual(alert_serializer.data['title'], title)
+        self.assertEqual(alert_serializer.data['description'], description)
